ac080db86d5c4fe7b9b3562bd9c9868a974b39be
Svetlana Lutkova <lilissiya@yandex.ru>
2023-08-31 16:07:57 +0500

    Исправить техошибку в названии папки

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router =require('express').Router();
-const {createUser} = require('../controllers/users');
+const {createUser, getUsers, getUser} = require('../controllers/users');
 router.post('/users', createUser);
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
+
+module.exports = router;
