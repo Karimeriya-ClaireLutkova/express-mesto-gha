ac21f0822e89640cb265407fbe8856c1ded96c29
Svetlana Lutkova <lilissiya@yandex.ru>
2023-08-31 23:46:48 +0500

    Создать тестового пользователя через postman

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const router = require('./routes/users');
+const bodyParser = require('body-parser');
 const {PORT = 3000} = process.env;
 const app = express();
-mongoose.connect('mongodb://localhost:27017/mestodb');
-app.use('/users', require('./routes/users'));
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb',{
+  useNewUrlParser: true
+});
+app.use(bodyParser.json());
+app.use(router);
 app.listen(PORT, () => {
   console.log(`App listening ${PORT}`)
-})
+});
