cc8b76aed8fe9dc7e62019ddb9e672dcc0669252
Svetlana Lutkova <lilissiya@yandex.ru>
2023-08-31 15:07:40 +0500

    Настроить роуты для пользователя

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const {PORT = 3000} = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use('/users', require('./routes/users'));
 app.listen(PORT, () => {
   console.log(`App listening ${PORT}`)
 })
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
 const router =require('express').Router();
 const {createUser} = require('../controllers/users');
 router.post('/users', createUser);
+router.get('/users', getUsers);
+router.get('/users/:userId', getUser);
