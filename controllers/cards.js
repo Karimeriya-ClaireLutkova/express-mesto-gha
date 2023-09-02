8d1d87f8f779df6af23730b268036b7dd58de3cc
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-02 23:26:56 +0500

    Создать добавление и удаление лайков

--- controllers/cards.js
+++ controllers/cards.js
@@ -24,3 +24,22 @@ module.exports.deleteCard = (req, res) => {
     })
     .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
 };
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {$addToSet: {likes: req.user._id}},
+    {new: true}
+  )
+  .then(card => res.send({ data: card }))
+  .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {$pull: {likes: req.user._id}},
+    {new: true}
+  )
+  .then(card => res.send({ data: card }))
+  .catch(err => res.status(500).send({message: 'Произошла ошибка'}));
+};
