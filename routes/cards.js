aa31b2d0db03f27d45530c6658dc64d1398131e6
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-03 20:44:47 +0500

    Доисправлять техошибки после проверки eslint

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,12 @@
-const router =require('express').Router();
-const {createCard, getCards, deleteCard, likeCard, dislikeCard} = require('../controllers/cards');
+const router = require('express').Router();
+const {
+  createCard,
+  getCards,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
+
 router.post('/cards', createCard);
 router.get('/cards', getCards);
 router.delete('/cards/:cardId', deleteCard);
