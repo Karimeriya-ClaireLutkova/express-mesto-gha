78f683e2bb7bd31661c6530f6ab007539a8e1a3b
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-03 20:53:54 +0500

    Добавить обработку неверного пути

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 app.use(routerUsers);
 app.use(routerCards);
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Ресурс не найден'));
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Ресурс не найден' });
 });
 app.listen(PORT);
