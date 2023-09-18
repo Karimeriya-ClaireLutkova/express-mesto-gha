85bfea0b1cd3a3eb7202a8bedf7aacae081baa79
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-18 21:49:07 +0500

    Добавить поиск пользователя по данным в схему user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema(
   {
@@ -36,5 +38,19 @@ const userSchema = new mongoose.Schema(
   },
   { versionKey: false },
 );
+userSchema.statics.findUserByCredentials = function(email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (user === null) {
+        return Promise.reject(new UnauthorizedError('Передан неверный логин или пароль'));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new UnauthorizedError('Передан неверный логин или пароль'));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
