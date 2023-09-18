63c22d2432131fa3c935937ced8535cef98d28b6
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-18 23:40:42 +0500

    Добавить передачу JWT в куки

--- controllers/users.js
+++ controllers/users.js
@@ -35,7 +35,7 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.send({ token });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
     })
     .catch(() => {
       res.status(401).send({ message: 'Передан неверный логин или пароль.' });
