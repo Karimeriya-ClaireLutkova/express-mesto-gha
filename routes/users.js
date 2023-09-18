7ffe06c89004f00e3dc55661681c4583c571aaad
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-19 01:08:16 +0500

    Создать роут для получения информации о пользователе

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ const {
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
+router.get('/users/me', getUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
 
