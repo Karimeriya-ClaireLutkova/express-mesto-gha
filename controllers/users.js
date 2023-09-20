47e02e54a749d215c3eb97ae48ccba7dfd891b14
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-20 13:36:33 +0500

    исправить ответ при авторизации

--- controllers/users.js
+++ controllers/users.js
@@ -44,6 +44,7 @@ module.exports.login = (req, res) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+      res.send({ jwt: token });
     })
     .catch(() => {
       res.status(401).send({ message: 'Передан неверный логин или пароль.' });
