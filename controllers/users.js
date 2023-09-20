ead0a3c23303f53a0f6c80c626df627c2a5f3e95
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-20 13:34:23 +0500

    Исправить авторизацию

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,6 @@ module.exports.createUser = (req, res, next) => {
       res.status(201).send(user);
     })
     .catch((err) => {
-      console.log(err);
       if (err.code === 11000) {
         next(new ConflictError('Пользователь с таким email уже существует'));
       }
@@ -44,7 +43,7 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
     })
     .catch(() => {
       res.status(401).send({ message: 'Передан неверный логин или пароль.' });
