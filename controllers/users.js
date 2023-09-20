411f1733bce608beda0ed0312e2e11504caff8fe
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-20 14:44:21 +0500

    Тест 12

--- controllers/users.js
+++ controllers/users.js
@@ -43,8 +43,8 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
       res.send({ jwt: token });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
     })
     .catch(() => {
       res.status(401).send({ message: 'Передан неверный логин или пароль.' });
