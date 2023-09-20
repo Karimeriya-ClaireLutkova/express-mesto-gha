f4a25fa9f0661ba5cabf0316fa62ba4eae8a2fc7
Svetlana Lutkova <lilissiya@yandex.ru>
2023-09-20 14:18:29 +0500

    Добавить в routes joi celebrate

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,11 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUser);
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.objectId(),
+  }),
+}), getUser);
 router.get('/users/me', getUser);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
@@ -16,6 +20,12 @@ router.patch('/users/me', celebrate({
     about: Joi.string().min(2).max(30),
   }).unknown(true),
 }), updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(
+      /^https?:\/\/(www\.)?([0-9a-zA-Z.-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpe?g|gif|png|bmp|webp)$/i,
+    ),
+  }),
+}), updateAvatar);
 
 module.exports = router;
